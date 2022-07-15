79163d9c0aec8dfcd7a136f3eb1a713662442fcc
opal1210 <109273473+opal1210@users.noreply.github.com>
2022-07-15 16:44:14 +0900

    상하좌우
    N값을 입력받아 N*N크기의 정사각형을 만든다. (왼쪽위의 좌표는(1,1), 오른쪽아래의 좌표는(N,N))
    시작은 (1,1)에서 시작한다.
    이동방향은 왼쪽 : 'L', 오른쪽 : 'R',  위쪽 : 'U', 아래쪾 : 'D'
    방향을 입력받아 그에 맞게 이동한다. (이동시 정사각형을 나가게되면 이동을 무시한다.)
    최종적으로 도달한 좌표값을 출력한다.

---
+++ updown.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+	int n = 0 ,row =1 , col = 1;
+	char de;
+	scanf_s("%d", &n);
+	while (1)
+	{
+		scanf_s(" %c", &de);
+		switch (de)
+		{
+		case 'R':
+			col++;
+			if (col > n)
+				col--;
+			continue;
+		case 'L':
+			col--;
+			if (col < 1)
+				col++;
+			continue;
+		case 'U':
+			row--;
+			if (row < 1)
+				row++;
+			continue;
+		case 'D':
+			row++;
+			if (row > n)
+				row--;
+			continue;
+		default:
+			break;
+		}
+		break;
+	}
+	printf("(%d, %d)", row, col);
+	return 0;
+}
